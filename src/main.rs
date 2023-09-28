300a884056558fd5fd17e661ff5e897e5e558c06
Zan <69921455+InZan123@users.noreply.github.com>
2023-09-28 15:51:57 +0200

    some stuf

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use poise::serenity_prelude as serenity;
+use poise::{serenity_prelude as serenity, PrefixFrameworkOptions};
 
-struct Data {} // User data, which is stored and accessible in all command invocations
-type Error = Box<dyn std::error::Error + Send + Sync>;
-type Context<'a> = poise::Context<'a, Data, Error>;
+mod read;
+pub struct Data {} // User data, which is stored and accessible in all command invocations
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+pub type Context<'a> = poise::Context<'a, Data, Error>;
 
 /// Displays your or another user's account creation date
 #[poise::command(slash_command, prefix_command)]
@@ -19,12 +20,14 @@ async fn age(
 #[tokio::main]
 async fn main() {
     let framework = poise::Framework::builder()
+        .token(read::read_token())
+        .intents(serenity::GatewayIntents::from_bits_truncate(3243775))
+
         .options(poise::FrameworkOptions {
             commands: vec![age()],
             ..Default::default()
         })
-        .token(std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN"))
-        .intents(serenity::GatewayIntents::non_privileged())
+        
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
