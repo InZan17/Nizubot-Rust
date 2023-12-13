7f9226f0144e63c74dcdb5a46474f216d4818ea6
Zan <69921455+InZan123@users.noreply.github.com>
2023-12-13 20:03:07 +0100

    /clear_data works now too

--- src/commands/admin_commands.rs
+++ src/commands/admin_commands.rs
@@ -1,12 +1,13 @@
 use super::Commands;
 
-//mod clear_data;
+mod clear_data;
 pub mod cotd_role;
 mod message;
 
 pub fn get_commands() -> Commands {
     return vec![
         message::message(),
-        cotd_role::cotdrole(), //clear_data::clear_data()
+        cotd_role::cotdrole(),
+        clear_data::clear_data()
     ];
 }
--- src/commands/admin_commands/clear_data.rs
+++ src/commands/admin_commands/clear_data.rs
@@ -1,18 +1,14 @@
 use crate::{Context, Error};
 
-/// Commands for messages.
+/// Clears all data I have on this guild/user. (Things such as reminders and other data will be reset.)
 #[poise::command(slash_command, default_member_permissions = "ADMINISTRATOR")]
 pub async fn clear_data(ctx: Context<'_>) -> Result<(), Error> {
-    let data = ctx.data();
-    let data = data
-        .storage_manager
-        .get_data_or_default::<String>(vec!["storing"], "".to_string())
-        .await;
-
-    *data.get_data_mut().await = write;
-
-    data.request_file_write().await;
-
-    ctx.say(format!("Data written!")).await?;
+    if let Some(guild_id) = ctx.guild_id() {
+        ctx.data().storage_manager.delete_data(vec!["guilds", guild_id.to_string().as_str()]).await;
+        ctx.reply("Successfully removed guild data.").await?;
+    } else {
+        ctx.data().storage_manager.delete_data(vec!["users", ctx.author().id.to_string().as_str()]).await;
+        ctx.reply("Successfully removed user data.").await?;
+    }
     Ok(())
 }
