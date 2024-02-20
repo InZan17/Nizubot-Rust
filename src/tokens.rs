d3e30e20198f965149baad735d08fc6f7fbbe864
Zan <69921455+InZan123@users.noreply.github.com>
2024-02-20 17:46:15 +0100

    Added example of new bot settings json

--- src/tokens.rs
+++ src/tokens.rs
@@ -1,5 +1,6 @@
 use std::fs;
 
+use poise::serenity_prelude::UserId;
 use serde::Deserialize;
 
 pub const BOT_SETTTINGS_FILE: &str = "./bot_settings.json";
@@ -8,6 +9,8 @@ pub const BOT_SETTTINGS_FILE: &str = "./bot_settings.json";
 pub struct BotSettings {
     pub discord_token: String,
     pub open_exchange_rates_token: String,
+    pub owner_user_ids: Vec<UserId>,
+    pub warning_webhook: String,
     pub logs_directory: String,
     pub temp_data_directory: String,
     pub surrealdb: SurrealDbSignInInfo,
